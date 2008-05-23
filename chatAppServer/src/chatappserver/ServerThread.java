946383ead0ef578fab7a7a7647b4fa552bc51bc4
Kea1337 <Kea1337@fea7f8c4-404d-0410-aa7f-dbd9a803abaa>
2008-05-23 20:56:28 +0000

    added login, checks username if logged in, checks for valid password, proceeds into main "chatting" session... also added functions to the thread...

--- chatAppServer/src/chatappserver/ServerThread.java
+++ chatAppServer/src/chatappserver/ServerThread.java
@@ -12,11 +12,21 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/*
+ * LOGN - login
+CHEK - check something (username available? nickname in use?)
+NEWA - new account
+CHAT - start chat
 
-/**
- *
- * @author kevin
- */
+You have to be in "CHAT" mode to use these, they should be sent with a '/' preceding them: /DISC etc.
+/DISC - Logout
+/PING - pings the user
+/WHOI - whois on user
+/TIME - returns the time on the user's end.
+
+ctrl+enter = disregard if a message starts with '/', just add another '/' so:
+/hi
+ * */
 public class ServerThread extends Thread {
     private Socket connectionSocket = null; 
     private Connection connectToMysql; 
@@ -25,111 +35,190 @@ public class ServerThread extends Thread {
         connectToMysql = sqlConnection; 
         connectionSocket = socket; 
     }
-    @Override
-    public void run() {
-        String query;
-        String reply = null;
+    
+    private void check(String query) {
         try {
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            query = inFromClient.readLine();
+            // checks database for dupliates
+            String parseQuery = query;
+            ResultSet results = null;
+            String reply = null;
             Statement sendSQLQuery = null;
             sendSQLQuery = connectToMysql.createStatement();
-            ResultSet results = null; 
-            if (query.startsWith("CHEK:")) {
-                // checks database for stuff
-                String parseQuery = query;
-                parseQuery = parseQuery.substring("CHEK:".length());
-                if (parseQuery.startsWith("nickname:")) {
+            
+            parseQuery = parseQuery.substring("CHEK:".length());
+            if (parseQuery.startsWith("nickname:")) {
+                // checks for duplicate nicknames
+                try {
+                    // checks for duplicate nicknames
                     parseQuery = parseQuery.substring("nickname:".length());
                     sendSQLQuery.executeQuery("SELECT nickname FROM user WHERE nickname = '" + parseQuery.toLowerCase() + "';");
                     results = sendSQLQuery.getResultSet();
                     boolean found = results.next();
-                    if (found)
+                    if (found) {
                         reply = "AlreadyExists\n";
-                    else
+                    } else {
                         reply = "Available\n";
+                    }
                     outToClient.writeBytes(reply);
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                else if (parseQuery.startsWith("username:")) {
+            } else if (parseQuery.startsWith("username:")) {
+                // check for duplicate usernames
+                try {
+                    // check for duplicate usernames
                     parseQuery = parseQuery.substring("username:".length());
                     sendSQLQuery.executeQuery("SELECT username FROM user WHERE username = '" + parseQuery.toLowerCase() + "';");
                     results = sendSQLQuery.getResultSet();
                     boolean found = results.next();
-                    if (found)
+                    if (found) {
                         reply = "AlreadyExists\n";
-                    else
+                    } else {
                         reply = "Available\n";
+                    }
                     outToClient.writeBytes(reply);
+                } catch (IOException ex) {
+                    Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                else {
-                    // some error about not knowing what's after the check
-                }
-                results.close();
-                sendSQLQuery.close();
-                inFromClient.close(); 
-                outToClient.close(); 
-                connectionSocket.close();
-                return; 
-            } // end check
-            else if (query.startsWith("NEWA:")) {
-                String parseQuery = query;
-                parseQuery = parseQuery.substring("NEWA:".length());
-                int i = parseQuery.indexOf(",");
-                String outQuery = "INSERT into user(username, email, fname, lname, password, nickname) Values('";
-
-                outQuery += parseQuery.substring(0, i)+"', '"; // get username
-                parseQuery = parseQuery.substring(i+1); // remove username
-                i = parseQuery.indexOf(","); // update comma location
-
-                outQuery += parseQuery.substring(0, i)+"', '"; // get email
-                parseQuery = parseQuery.substring(i+1); // remove email
-                i = parseQuery.indexOf(","); // update comma location
-
-                outQuery += parseQuery.substring(0, i)+"', '"; // get fname
-                parseQuery = parseQuery.substring(i+1); // remove fname
-                i = parseQuery.indexOf(","); // update comma location
-
-                outQuery += parseQuery.substring(0, i)+"', '"; // get lname
-                parseQuery = parseQuery.substring(i+1); // remove lname
-                i = parseQuery.indexOf(","); // update comma location
-
-                outQuery += parseQuery.substring(0, i)+"', '"; // get password
-                parseQuery = parseQuery.substring(i+1); // remove password
-                i = parseQuery.indexOf(","); // update comma location  
-
-                outQuery += parseQuery+"');"; // get nickname
+            } else {
                 
-                /*
-                System.out.println(parseQuery); 
-                System.out.println(outQuery); 
-                if (parseQuery.isEmpty()) 
-                    System.out.println("parse should be correct... (debug info) "); 
-                */
-                sendSQLQuery.execute(outQuery); 
-                // check if query was executed succesfully and give feedback... 
-                // outToClient.writeBytes("feedback_here..."); 
-                // we decided this is not necessary. 
-                sendSQLQuery.close();
-                inFromClient.close(); 
-                outToClient.close(); 
-                connectionSocket.close();
-                return; 
-            } // end new account 
-            //reply = query.toUpperCase() + '\n';
-            if(query.equalsIgnoreCase("DISC")){
-               // we have to do sql query that we're not online anymore. connectionSocket.close() has to be done anyway. 
-               System.out.println("Connection Socket Quitting");
-            } 
-            else {
-                //outToClient.writeBytes(reply);
             }
+            results.close();
+            sendSQLQuery.close();
+            inFromClient.close();
+            outToClient.close();
+            return;
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
-        
+    } // end check 
+    
+    private void newaccnt(String query) {
+        try {
+            Statement sendSQLQuery = null;
+            sendSQLQuery = connectToMysql.createStatement();
+            String parseQuery = query;
+            parseQuery = parseQuery.substring("NEWA:".length());
+            int i = parseQuery.indexOf(",");
+            String outQuery = "INSERT into user(username, email, fname, lname, password, nickname) Values('";
+
+            outQuery += parseQuery.substring(0, i) + "', '"; // get username
+            parseQuery = parseQuery.substring(i + 1); // remove username
+            i = parseQuery.indexOf(","); // update comma location
+            outQuery += parseQuery.substring(0, i) + "', '"; // get email
+            parseQuery = parseQuery.substring(i + 1); // remove email
+            i = parseQuery.indexOf(","); // update comma location
+            outQuery += parseQuery.substring(0, i) + "', '"; // get fname
+            parseQuery = parseQuery.substring(i + 1); // remove fname
+            i = parseQuery.indexOf(","); // update comma location
+            outQuery += parseQuery.substring(0, i) + "', '"; // get lname
+            parseQuery = parseQuery.substring(i + 1); // remove lname
+            i = parseQuery.indexOf(","); // update comma location
+            outQuery += parseQuery.substring(0, i) + "', '";
+            parseQuery = parseQuery.substring(i + 1); // remove password
+            i = parseQuery.indexOf(","); // update comma location
+            outQuery += parseQuery + "');"; // get nickname
+            sendSQLQuery.execute(outQuery);
+
+            // outToClient.writeBytes("feedback_here...");
+            // we decided this is not necessary.
+            sendSQLQuery.close();
+            return;
+        } catch (SQLException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } // end new account 
+    
+    private void login(String query) {
+        Statement sendSQLQuery = null;
+        String reply = null;
+        String parseQuery = query; 
+        try {
+            sendSQLQuery = connectToMysql.createStatement();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            parseQuery = query.substring("LOGN:".length(),query.indexOf(",")); // holds username
+            ResultSet results = null;
+            sendSQLQuery.executeQuery("SELECT username FROM threadlookup WHERE username = '" + parseQuery + "';");
+            results = sendSQLQuery.getResultSet();
+            boolean login = false; 
+            boolean found = results.next();
+            if (found) {
+                reply = "ERR1\n"; // user is already logged in
+            } else {
+                // check password
+                sendSQLQuery.executeQuery("SELECT username FROM user WHERE username = '" + parseQuery + "' AND password = '" + query.substring(query.indexOf(",")) + "';");
+                boolean validCredentials = results.next(); 
+                if (!validCredentials) {
+                    // either username doesn't exist, or the password is invalid..
+                    reply = "ERR\n"; 
+                }
+                else {
+                    login = true; 
+                    reply = "SUCC\n";
+                    sendSQLQuery.execute("INSERT into threadlookup(username, threadid) Values('"+query+"', '"+this.getId()+"');"); // register this thread with this username. 
+                    // we won't close the thread anymore until the user disconnects. 
+                }
+            }
+            outToClient.writeBytes(reply);
+            /*
+             we're now in the chatting section... 
+             */
+            
+            
+            if (login) {
+                while (!inFromClient.readLine().equals("/DISC")) {
+                    System.out.println("not disconnected! "); 
+
+                }
+                
+                outToClient.writeBytes("DISC");  // tell cleint we're discing
+                System.out.println("Disconnecting..."); // debug
+            }
+            
+            inFromClient.close(); 
+            outToClient.close(); 
+            sendSQLQuery.close(); 
+            results.close(); 
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @Override
+    public void run() {
+        try {
+            String query;
+            try {
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                query = inFromClient.readLine();
+                if (query.startsWith("CHEK:")) { // check for duplicates
+                    check(query);
+                } else if (query.startsWith("NEWA:")) { // new account 
+                    newaccnt(query);
+                } else if (query.equalsIgnoreCase("LOGN")) { // login
+                    login(query); 
+                } else {
+                    // error, invalid code. 
+                }
+                inFromClient.close(); 
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            connectionSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     } // end run
 }
